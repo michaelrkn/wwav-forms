4920383a9e3fa023851a7fa26122e27e30bbcde0
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2020-07-20 15:40:23 -0500

    improve spacing and indentation for readability

--- parser.js
+++ parser.js
@@ -1,59 +1,40 @@
 var myPostRender = function(args) {
   fetch('./formtest_files/autodata.json')
-    .then(
-      function(response) {
+  .then(function(response) {
+    response.json().then(function(sections) {
+      for (var i = 0; i < sections.length; i++) {
+        var section = sections[i].data;
+        var sectionFields= sections[i].fields;
 
-        response.json().then(function(sections) {
-
-          for (var i = 0; i < sections.length; i++) {
-
-
-             var section = sections[i].data;
-             var sectionFields= sections[i].fields;
-
-             autocomplete({
-               minLength: 1,
-               input: document.querySelector(sections[i].label.type+'[title="'+sections[i].label.loc+'"]'),
-               fetch: function(text, update) {
-                 text = text.toLowerCase();
-                 var suggestions = section.filter(n => n.label.toLowerCase().match(text))
-                 update(suggestions);
-               },
-               onSelect: function (item, inputfield) {
-
-                 for (field in item) {
-                   if (field !== 'label' && field in sectionFields){
-                     if (sectionFields[field].type==='input') {
-
-                       document.querySelector(sectionFields[field].type+'[title="'+sectionFields[field].loc+'"]').value=item[field];
-
-                     } else if (sectionFields[field].type==='select') {
-
-                       var options=Array.from(document.querySelector(sectionFields[field].type+'[title="'+sectionFields[field].loc+'"]').children).filter(function(option){ // to find the text that matches the State Value
-
-                         return option.text == item[field];
-                       });
-
-                       options[0].selected=true;
-
-                     }
-                   }
-                 }
-                 inputfield.value = item.label;
-
-               },
-               preventSubmit: true,
-             });
-
-
-          }
-
+        autocomplete(
+          {
+            minLength: 1,
+            input: document.querySelector(sections[i].label.type+'[title="'+sections[i].label.loc+'"]'),
+            fetch: function(text, update) {
+              text = text.toLowerCase();
+              var suggestions = section.filter(n => n.label.toLowerCase().match(text))
+              update(suggestions);
+          },
+          onSelect: function (item, inputfield) {
+            for (field in item) {
+              if (field !== 'label' && field in sectionFields){
+                if (sectionFields[field].type==='input') {
+                  document.querySelector(sectionFields[field].type+'[title="'+sectionFields[field].loc+'"]').value=item[field];
+                } else if (sectionFields[field].type==='select') {
+                  var options=Array.from(document.querySelector(sectionFields[field].type+'[title="'+sectionFields[field].loc+'"]').children).filter(function(option){ // to find the text that matches the State Value
+                    return option.text == item[field];
+                  });
+                  options[0].selected=true;
+                }
+              }
+            }
+            inputfield.value = item.label;
+          },
+          preventSubmit: true,
         });
       }
-    );
-
-
-
+    });
+  });
   return args;
 };
 
